8aae877aefa73f6841870ab4629d23bbd0c269ad
Bruno Grande <bruno.grande@sagebase.org>
2021-07-07 20:16:00 +0000

    Update Rstudio Docker setup

---
+++ rstudio/bin/add_users.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Batch-create UNIX users for all students and admins.
+#
+# $1 = Path to users CSV file (see '../conf/users.csv' for format)
+#
+# Usage: 
+#   add_users.sh users.csv
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+while read line; do
+  a=($(echo "$line" | tr ',' '\n'))
+  b=($(echo "${a[2]}" | tr ';' '\n'))
+  getent passwd "${a[0]}" > /dev/null
+  if [ $? -eq 0 ]; then
+    echo "User ${a[0]} already exists; skipping."
+  else
+    # add the user
+    user="${a[0]}"
+    userpass="${a[1]}"
+    groupstr=$(join_by , "${b[@]}")
+    echo "Adding user $user to groups $groupstr"
+    useradd -m -p $(openssl passwd -crypt $userpass) -s /bin/bash $user
+    usermod -a -G $groupstr $user
+  fi
+done < "$1"
---
+++ rstudio/bin/broadcast_module.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copy a module folder to all user home directories.
+#
+# $1 = Module folder in shared space to be copied
+# $2 = UNIX group name
+#
+# Usage:
+#   broadcast_module.sh /shared/modules/module0 rstudio-user
+
+SHARED_MODULE=$(realpath $1)
+MODULE_NAME=$(basename $SHARED_MODULE)
+GROUP=$2
+SKIP_USER=$3
+
+GROUP_MEMBERS=$(getent group $GROUP)
+GROUP_MEMBERS=$(echo ${GROUP_MEMBERS##*:} | tr "," "\n")
+for user in $GROUP_MEMBERS; do
+    if [[ "$user" != "$SKIP_USER" ]]; then
+        echo "Copying '${SHARED_MODULE}' to user: ${user}"
+        user_home="/home/${user}"
+        user_modules="${user_home}/modules"
+        if [[ ! -e "$user_modules" ]]; then
+            sudo mkdir -p $user_modules
+        fi
+
+        sudo rsync -ur $SHARED_MODULE $user_modules
+        sudo chown -R "${user}":rstudio-admin $user_modules
+        sudo find "${user_modules}/${MODULE_NAME}" \
+            -name "session-persistent-state" \
+            | sudo xargs -n 1 -I{} rm -f {}
+        sudo find "${user_modules}/${MODULE_NAME}" \
+            -type d \
+            -name ".Rproj.user" \
+            | xargs -n 1 -I {} sudo rm -rf {}
+        sudo rm -f ${user_modules}/${MODULE_NAME}/*.nb.html
+    fi
+done
